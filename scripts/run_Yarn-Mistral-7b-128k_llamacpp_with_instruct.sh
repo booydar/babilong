75eaa23f3b8ae2448343d7bb38d0d99063070924
yurakuratov <9271630+yurakuratov@users.noreply.github.com>
2024-05-24 21:54:10 +0300

    add scripts and eval results for LLaMA-2-7B-32K and Yarn-Mistral-7b-128k

---
+++ scripts/run_Yarn-Mistral-7b-128k_llamacpp_with_instruct.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# CUDA_VISIBLE_DEVICES=0 ./script_name.sh
+set -e
+
+RESULTS_FOLDER="./babilong_evals"
+MODEL_NAME="NousResearch/Yarn-Mistral-7b-128k"
+
+USE_CHAT_TEMPLATE=false
+USE_INSTRUCTION=true
+USE_EXAMPLES=true
+USE_POST_PROMPT=true
+API_URL="http://localhost:8082/completion"
+
+DATASET_NAME="booydar/babilong-1k-samples"
+TASKS=("qa1" "qa2" "qa3" "qa4" "qa5")
+LENGTHS=("0k" "1k" "2k" "4k" "8k" "16k" "32k")
+
+# setup llamacpp server with
+# server -b 2048 -ub 2048 -fa -n 15 -ngl 99 -c 131072 --port 8082 -m ~/models/Yarn-Mistral-7b-128k/Yarn-Mistral-7b-128k.Q8_0.gguf
+
+echo running $MODEL_NAME on "${TASKS[@]}" with "${LENGTHS[@]}"
+
+python scripts/run_model_on_babilong.py \
+    --results_folder "$RESULTS_FOLDER" \
+    --dataset_name "$DATASET_NAME" \
+    --model_name "$MODEL_NAME" \
+    --tasks "${TASKS[@]}" \
+    --lengths "${LENGTHS[@]}" \
+    $( [ "$USE_CHAT_TEMPLATE" == true ] && echo "--use_chat_template" ) \
+    $( [ "$USE_INSTRUCTION" == true ] && echo "--use_instruction" ) \
+    $( [ "$USE_EXAMPLES" == true ] && echo "--use_examples" ) \
+    $( [ "$USE_POST_PROMPT" == true ] && echo "--use_post_prompt" ) \
+    --api_url "$API_URL"
+
+DATASET_NAME="booydar/babilong-samples"
+TASKS=("qa1" "qa2" "qa3" "qa4" "qa5")
+LENGTHS=("64k" "128k")
+
+echo running $MODEL_NAME on "${TASKS[@]}" with "${LENGTHS[@]}"
+
+python scripts/run_model_on_babilong.py \
+    --results_folder "$RESULTS_FOLDER" \
+    --dataset_name "$DATASET_NAME" \
+    --model_name "$MODEL_NAME" \
+    --tasks "${TASKS[@]}" \
+    --lengths "${LENGTHS[@]}" \
+    $( [ "$USE_CHAT_TEMPLATE" == true ] && echo "--use_chat_template" ) \
+    $( [ "$USE_INSTRUCTION" == true ] && echo "--use_instruction" ) \
+    $( [ "$USE_EXAMPLES" == true ] && echo "--use_examples" ) \
+    $( [ "$USE_POST_PROMPT" == true ] && echo "--use_post_prompt" ) \
+    --api_url "$API_URL"
